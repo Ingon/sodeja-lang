e952f053546a43b9407f812d9c51e2c748cb8061
sungon <sungon@f1957b57-f328-0410-a4b2-43a64d9af605>
2007-02-08 11:09:37 +0000

    Changed code

---
+++ sodeja-lang/src/org/sodeja/model/LocalizableResource.java
@@ -0,0 +1,14 @@
+package org.sodeja.model;
+
+import java.util.Collection;
+import java.util.Locale;
+
+public interface LocalizableResource {
+	public String getId();
+	public void setId(String id);
+	
+	public String getLocalizedValue(Locale locale);
+	public void setLocalizedValue(Locale locale, String str);
+	public Collection<Locale> getAvailableLocales();
+
+}
