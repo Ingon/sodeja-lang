bcd8736df51b3643a0e03055f6178896a551f719
sungon <sungon@f1957b57-f328-0410-a4b2-43a64d9af605>
2008-01-16 13:06:19 +0000

    Image is now loading through files!

--- sodeja-lang/src/org/sodeja/files/FileUtils.java
+++ sodeja-lang/src/org/sodeja/files/FileUtils.java
@@ -1,11 +1,14 @@
 package org.sodeja.files;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 public class FileUtils {
 	public static byte[] readFile(File file) throws FileNotFoundException, IOException {
@@ -19,5 +22,21 @@ public class FileUtils {
 		}
 		
 		return out.toByteArray();
+	}
+
+	public static String readFully(String file) throws IOException {
+		return readFully(new FileReader(file));
+	}
+	
+	public static String readFully(Reader reader) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(reader);
+		
+		for(String str = br.readLine();str != null;str = br.readLine()) {
+			sb.append(str);
+			sb.append("\r\n");
+		}
+		
+		return sb.toString();
 	}
 }
