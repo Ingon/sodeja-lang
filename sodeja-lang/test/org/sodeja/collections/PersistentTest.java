8ecab11bf1db9ff43efb7fedd7edcfa6facb9335
sungon <sungon@f1957b57-f328-0410-a4b2-43a64d9af605>
2009-10-18 21:32:25 +0000

    more tests

--- sodeja-lang/test/org/sodeja/collections/PersistentTest.java
+++ sodeja-lang/test/org/sodeja/collections/PersistentTest.java
@@ -1,6 +1,9 @@
 package org.sodeja.collections;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 public class PersistentTest {
@@ -50,10 +53,57 @@ public class PersistentTest {
 	}
 	
 	public static void main(String[] args) {
-		testMulty();
+//		testMulty();
 //		testIterator();
+		testPerformance();
 	}
 
+	private static final int GEN_SIZE = 50000;
+	private static void testPerformance() {
+		System.out.println("Hash: " + testHash());
+		System.out.println("Tree: " + testTree());
+		System.out.println("Pers: " + testPersistent());
+	}
+	
+	private static long testHash() {
+		long start = System.currentTimeMillis();
+		Set<String> set = new HashSet<String>();
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set.add("data" + i);
+		}
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set.remove("data" + i);
+		}
+		long end = System.currentTimeMillis();
+		return (end - start);
+	}
+
+	private static long testTree() {
+		long start = System.currentTimeMillis();
+		Set<String> set = new TreeSet<String>();
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set.add("data" + i);
+		}
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set.remove("data" + i);
+		}
+		long end = System.currentTimeMillis();
+		return (end - start);
+	}
+	
+	private static long testPersistent() {
+		long start = System.currentTimeMillis();
+		PersistentSet<String> set = new PersistentSet<String>();
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set = set.addValue("data" + i);
+		}
+		for(int i = 0; i < GEN_SIZE; i++) {
+			set = set.removeValue("data" + i);
+		}
+		long end = System.currentTimeMillis();
+		return (end - start);
+	}
+	
 	private static void testIterator() {
 		PersistentMap<String, String>[] maps = new PersistentMap[30];
 		maps[0] = new PersistentMap<String, String>();
