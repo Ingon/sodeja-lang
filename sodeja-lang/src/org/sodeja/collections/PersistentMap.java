64286fc420dbe2f3f646967c96a918c456566789
sungon <sungon@f1957b57-f328-0410-a4b2-43a64d9af605>
2009-10-22 10:19:49 +0000

    Testing vs Clojure PersistentHashSet

--- sodeja-lang/src/org/sodeja/collections/PersistentMap.java
+++ sodeja-lang/src/org/sodeja/collections/PersistentMap.java
@@ -4,12 +4,10 @@ import java.util.AbstractSet;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.Map.Entry;
 
 import org.sodeja.functional.Pair;
 import org.sodeja.lang.MutableInteger;
