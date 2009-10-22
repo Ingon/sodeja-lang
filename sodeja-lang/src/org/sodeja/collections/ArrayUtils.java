bed0c0d9fe8525feab95f1ac45e6e9cc8395d2bc
sungon <sungon@f1957b57-f328-0410-a4b2-43a64d9af605>
2009-10-22 09:56:18 +0000

    Faster putAll impl

--- sodeja-lang/src/org/sodeja/collections/ArrayUtils.java
+++ sodeja-lang/src/org/sodeja/collections/ArrayUtils.java
@@ -216,4 +216,15 @@ public final class ArrayUtils {
     	builder.append("]");
     	return builder.toString();
     }
+    
+    public static int compare(int[] a, int[] b) {
+    	int cmpLen = a.length > b.length ? b.length : a.length;
+    	for(int i = 0; i < cmpLen; i++) {
+    		if(a[i] == b[i]) {
+    			continue;
+    		}
+    		return a[i] - b[i];
+    	}
+    	return a.length - b.length;
+    }
 }
